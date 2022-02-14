908622d5c2093868668738cd7b3d6b2482e2e70c
yadhukrishnapp <86195118+yadhukrishnapp@users.noreply.github.com>
2022-02-14 16:52:25 +0530

    Template CPP

---
+++ Template.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1e9+7
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) cc.begin(), c.end()
+#define mini(a, b, c) min(c, min(a, b))
+#define mini(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+int32_t main()
+{
+    fast
+
+    // Code here
+
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+
+    }
+    return 0;
+}
